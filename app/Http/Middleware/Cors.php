a266e13331667cd13f241c6eba324a7e2a7dda07
kamini pawar <pawarkamini28@gmail.com>
2019-08-22 15:35:37 +0530

    initialcode
    colorwale

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+        ->header('Access-Control-Allow-Origin','localhost:8000')
+        ->header('Access-Control-Allow-headers', 'X-REQUESTED-WITH');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+// Route::get('faq', function () {
+// 	return [
+// 		'series' => 200,
+// 		'lessons' => 1300
+// 	];
+	
+// });
+
+use Illuminate\Http\Request;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/posts', 'PostController@index');
+Route::post('/post/create', 'PostController@store');
+Route::get('/post/edit/{id}', 'PostController@edit');
+Route::post('/post/update/{id}', 'PostController@update');
+Route::delete('/post/delete/{id}', 'PostController@delete');
